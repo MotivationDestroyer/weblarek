6e438908912781fa5e4929fa8d14e0f6dadd8668
Oleg <motivationdestroyer@gmail.com>
2026-08-20 12:40:53 +0300

    9 спринт продолжение ларька

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,6 @@ import { Basket } from './components/models/Basket';
 import { Buyer } from './components/models/Buyer';
 
 import { API_URL } from './utils/constants';
-import { apiProducts } from './utils/data';
-import { IOrder } from './types';
 
 const api = new Api(API_URL);
 const larekApi = new LarekApi(api);
@@ -17,116 +15,10 @@ const productsModel = new Products();
 const basketModel = new Basket();
 const buyerModel = new Buyer();
 
-console.log('Проверка моделей и API');
-
-productsModel.setItems(apiProducts.items);
-
-console.log('Каталог товаров:', productsModel.getItems());
-
-console.log(
-	'Товар по id:',
-	productsModel.getItem(apiProducts.items[0].id)
-);
-
-productsModel.setPreview(apiProducts.items[0].id);
-
-console.log(
-	'Товар для подробного просмотра:',
-	productsModel.getPreview()
-);
-
-basketModel.addItem(apiProducts.items[0]);
-basketModel.addItem(apiProducts.items[1]);
-
-console.log('Товары в корзине:', basketModel.getItems());
-
-console.log(
-	'Общая стоимость товаров:',
-	basketModel.getTotal()
-);
-
-console.log(
-	'Количество товаров:',
-	basketModel.getCount()
-);
-
-console.log(
-	'Первый товар находится в корзине:',
-	basketModel.hasItem(apiProducts.items[0].id)
-);
-
-basketModel.removeItem(apiProducts.items[0].id);
-
-console.log(
-	'Корзина после удаления товара:',
-	basketModel.getItems()
-);
-
-console.log(
-	'Количество товаров после удаления:',
-	basketModel.getCount()
-);
-
-buyerModel.setBuyer({
-	payment: 'online',
-	address: 'Муром',
-	email: 'test@test.ru',
-	phone: '+79999999999',
-});
-
-console.log(
-	'Данные покупателя:',
-	buyerModel.getBuyer()
-);
-
-console.log(
-	'Ошибки валидации (валидные данные):',
-	buyerModel.validate()
-);
-
-buyerModel.clear();
-
-console.log(
-	'Ошибки валидации (невалидные данные):',
-	buyerModel.validate()
-);
-
-console.log(
-	'Покупатель после очистки:',
-	buyerModel.getBuyer()
-);
-
-basketModel.clear();
-
-console.log(
-	'Корзина после очистки:',
-	basketModel.getItems()
-);
-
-larekApi
-	.getProducts()
+larekApi.getProducts()
 	.then((data) => {
 		productsModel.setItems(data.items);
-
-		console.log(
-			'Каталог, полученный с сервера:',
-			productsModel.getItems()
-		);
-
-		const order: IOrder = {
-			...buyerModel.getBuyer(),
-			items: [],
-			total: 0,
-		};
-
-		console.log(
-			'Объект заказа для отправки:',
-			order
-		);
 	})
 	.catch((error) => {
-		console.error(
-			'Ошибка при получении каталога:',
-			error
-		);
+		console.error('Ошибка при получении каталога:', error);
 	});
